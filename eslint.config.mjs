4f7fd6f907e05f5b9ce2b333fd356283bbf34473
mikeogawa <mike.ogawa@techmo.jp>
2025-09-30 15:26:54 +0900

    add config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,14 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     "rules": {
-      "@typescript-eslint/no-explicit-any": "off"
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off", 
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-return": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/prefer-as-const": "off",
+      "react/no-unescaped-entities": "off"
     },
     ignores: [
       "node_modules/**",
